a646e38ab88be68771750c9f0ea7a8c6aa4f68c4
jalfonso <jalfonso@grupodot.com>
2019-05-07 18:48:46 -0500

    Files not used deleted

--- test.py
+++
@@ -1,5 +0,0 @@
-import httplib
-conn = httplib.HTTPConnection("http://34.66.7.201:8080/job/microservice-environments-set/parambuild?token=123456&NAME=test-micro&ENV=test-env&NS=testnamespace")
-conn.request("HEAD","/index.html")
-res = conn.getresponse()
-print res.status, res.reason
